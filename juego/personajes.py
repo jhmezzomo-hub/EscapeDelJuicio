2489a30dac3fbfe1ff7aba785e463dcb2bac0bd3
idavirro <jmezzomo@alumno.huergo.edu.ar>
2025-08-27 07:48:42 -0300

    elimine las carpetas innecesarias

--- juego/personajes.py
+++
@@ -1,7 +0,0 @@
-# personajes.py
-"""
-Módulo para manejar la información de los personajes del juego.
-Contiene datos y funciones para acceder y mostrar información.
-"""
-
-# Datos de los personajes:
