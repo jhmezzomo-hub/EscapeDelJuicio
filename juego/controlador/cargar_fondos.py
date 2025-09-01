24115534add904d2c260d21a0c55ae547598a93b
Rocco.n.c <jcatoraz@alumno.huergo.edu.ar>
2025-09-01 11:12:41 -0300

    cambie la imagen de fondo

--- juego/controlador/cargar_fondos.py
+++ juego/controlador/cargar_fondos.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from controlador.rutas import rutas_img
 
 def cargar_fondo(WIDTH, HEIGHT):
-    path = rutas_img("Fondo_Juego.png")
+    path = rutas_img("Fondo_inicial.png")
     fondo = pygame.image.load(path)
     fondo = pygame.transform.scale(fondo, (WIDTH, HEIGHT))
     return fondo
