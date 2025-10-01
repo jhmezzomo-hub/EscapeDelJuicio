10c41a11d2613242eabac22b0061a878c1340fe3
sabiondo298 <ncomin@alumno.huergo.edu.ar>
2025-10-01 09:05:02 -0300

    creé la función para crear los textos

--- juego/salas/salas_inicio.py
+++ juego/salas/salas_inicio.py
@@ -11,6 +11,22 @@ from juego.ui.inventory import Inventory
 # Importamos la sala dos
 from juego.salas.sala2 import iniciar_sala2
 
+def bienvenida_textos(tiempo_actual, tiempo_inicio, fuente, screen, fondo, personaje, personaje_rect):
+    mensajes = [
+        "Bienvenidos al Escape del Juicio",
+        "Este es un juego de vida o muerte en el que te enfrentarás a desafíos mortales",
+        "Tendrás que derrotar enemigos, resolver acertijos y escapar con vida",
+        "Podrás escapar?"
+    ]
+    for i, mensaje in enumerate(mensajes):
+        if tiempo_actual - tiempo_inicio < (i + 1) * 2000:
+            texto_bienvenida = fuente.render(mensaje, True, (255, 255, 255))
+            screen.blit(fondo, (0, 0))
+            screen.blit(personaje, personaje_rect)
+            screen.blit(texto_bienvenida, (screen.get_width() // 2 - texto_bienvenida.get_width() // 2, 600 - 70))
+            pygame.display.flip()
+            return True
+    return False
 
 def iniciar_sala():
     pygame.init()
