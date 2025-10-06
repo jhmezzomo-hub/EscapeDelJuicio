02f5384b46ed107994a414f597feb780a7601f91
luca <jmezzomo@alumno.huergo.edu.ar>
2025-10-06 08:08:56 -0300

    agregue para ensombrecer la pantalla

---
+++ juego/pantalla/ensombrecer.py
@@ -0,0 +1,13 @@
+import pygame, sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from info_pantalla.info_pantalla import info_pantalla
+
+def ensombrecer():
+    screen = info_pantalla()
+    sombra = pygame.Surface((screen.get_width(), screen.get_height()))  # Mismo tamaño que la pantalla
+    sombra.set_alpha(180)  # Nivel de transparencia (0 = transparente, 255 = opaco)
+    sombra.fill((0, 0, 0))  # Color negro
+
+    # Dibujas la sombra encima del fondo
+    screen.blit(sombra, (0, 0))
