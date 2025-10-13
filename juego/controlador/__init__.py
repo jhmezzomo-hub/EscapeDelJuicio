acb298a7b85dadcef5fc3540479f7fc04dea8d25
jhmezzomo-hub <jmezzomo@alumno.huergo.edu.ar>
2025-10-13 11:38:03 -0300

    saque imports inutiles de init

--- juego/controlador/__init__.py
+++ juego/controlador/__init__.py
@@ -1,7 +1,5 @@
 # Make controlador a Python package
-from juego.controlador.controles import manejar_mc
 from juego.limite_colisiones.colision_piso import colision_piso
 from juego.controlador.rutas import rutas_img, rutas_data, rutas_juego
-from juego.controlador.cargar_personaje import crear_personaje
 from juego.controlador.cargar_fondos import cargar_fondo
 from juego.limite_colisiones.crear_mascara import crear_mascara
