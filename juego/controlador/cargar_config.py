7b876b23485c8ab5781a08c19c35cf4afb84b787
ncomin-a <ncomin@alumno.huergo.edu.ar>
2025-10-15 11:49:23 -0300

    sigo con la config para hacer luego las siguientes salas

--- juego/controlador/cargar_config.py
+++ juego/controlador/cargar_config.py
@@ -32,6 +32,38 @@ def get_config_sala(nombre_sala):
             
             "sala_anterior": "inicio",
             "siguiente_sala": "sala3"
+        },
+        "sala3": {
+            "fondo": "Fondo_sala3.png",
+            "nombre_carpeta": "Fondos",
+            "personaje": {
+                "pos_inicial": (100, 400),
+                "tamaño": (125, 150),
+            },
+            "caption": "Sala 3",
+            "puertas": {
+                "volver": pygame.Rect(100, 400, 70, 40),
+                "salida": pygame.Rect(400, 400, 70, 40)
+            },
+            
+            "sala_anterior": "sala2",
+            "siguiente_sala": "sala4"
+        },
+        "sala4": {
+            "fondo": "Fondo_sala1.png",
+            "nombre_carpeta": "Fondos",
+            "personaje": {
+                "pos_inicial": (400, 400),
+                "tamaño": (125, 150),
+            },
+            "caption": "Sala 3",
+            "puertas": {
+                "volver": pygame.Rect(400, 400, 70, 40),
+                "salida": pygame.Rect(770, 400, 70, 40)
+            },
+            
+            "sala_anterior": "sala3",
+            "siguiente_sala": "sala5"
         }
     }
     return configs.get(nombre_sala, None)
