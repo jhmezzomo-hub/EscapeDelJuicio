adc873f68c851a1e61be48ef8ef30046bf2b80bb
ncomin-a <ncomin@alumno.huergo.edu.ar>
2025-10-20 08:16:34 -0300

    arreglé main

--- juego/main.py
+++ juego/main.py
@@ -2,20 +2,20 @@ import pygame, sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from juego.pantalla.pantalla_inicio import pantalla_de_inicio
-from juego.salas.salas_inicio import iniciar_sala
-from juego.salas.sala2 import iniciar_sala2
+from juego.salas.cargar_salas import cargar_sala
 from juego.salas.sala_mensaje import sala_mensaje_bienvenida
 
 def main():
-    print("Iniciando juego desde main.py...")
+    print("Iniciando juego desde main.py...")    
+    pygame.init()
     pantalla_de_inicio()  # Mostrar menú principal
-    sala_actual = "sala1"
-    sala_mensaje_bienvenida()  # Mostrar mensaje de bienvenida
+    sala_actual = "inicio"
+    #sala_mensaje_bienvenida()  # Mostrar mensaje de bienvenida
     while True:
-        if sala_actual == "sala1":
-            siguiente = iniciar_sala()
+        if sala_actual == "inicio":
+            siguiente = cargar_sala("inicio")
         elif sala_actual == "sala2":
-            siguiente = iniciar_sala2()
+            siguiente = cargar_sala("sala2")
         else:
             break  # Termina el juego si no hay más salas
 
