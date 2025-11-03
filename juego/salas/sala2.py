4328a9744763c6fbe1d8b4c14d9ed573ef4c8d55
JLGL <jmezzomo@alumno.huergo.edu.ar>
2025-11-03 08:53:22 -0300

    agregue la imagen de una linterna al inventario

--- juego/salas/sala2.py
+++ juego/salas/sala2.py
@@ -77,8 +77,10 @@ def iniciar_sala2(inv=None):
     personaje, personaje_rect = general["personaje"], general["personaje_rect"]
     fuente = general["fuente"]
 
-    linterna_item = Item(type="linterna", count=1, max_stack=1, color=(255, 255, 150), image=None)
-    inv.inventory_slots[0] = linterna_item
+    # Cargar imagen de la linterna
+    linterna_img, _ = cargar_personaje("linterna.png", "inventario_obj", size, (64, 64))
+    linterna_item = Item(type="linterna", count=1, max_stack=1, color=(255, 255, 150), image=linterna_img)
+    inv.inventory_slots[1] = linterna_item
 
     mostrar_hitboxes = False
     mostrar_malo = False
