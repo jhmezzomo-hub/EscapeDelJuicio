7d2cb7a0bf4637af71015d06cd29f1d960c516e8
JLGL <jmezzomo@alumno.huergo.edu.ar>
2025-11-10 10:27:16 -0300

    agregue para evitar errores

--- juego/salas/sala3.py
+++ juego/salas/sala3.py
@@ -5,6 +5,8 @@ from juego.salas.cargar_salas import cargar_sala
 from juego.controlador.inventario import crear_inventario
 
 def iniciar_sala3(inv):
+    if inv == None:
+        inv = crear_inventario()
     return cargar_sala("sala3", maniquies=[], inv=inv)
 
 if __name__ == "__main__":
