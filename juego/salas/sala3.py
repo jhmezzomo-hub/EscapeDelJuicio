dda5d328bf8b9cb76d196162f52a48ff72fb103e
jhmezzomo-hub <jmezzomo@alumno.huergo.edu.ar>
2025-11-14 01:26:45 -0300

    agregue que las pos_inicial sea la esquina inferior de la puerta izuqierdaen
    NO FUNICONA

--- juego/salas/sala3.py
+++ juego/salas/sala3.py
@@ -3,10 +3,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from juego.salas.cargar_salas import cargar_sala
 from juego.controlador.inventario import crear_inventario
+from juego.controlador.cargar_config import get_config_sala
 
 def iniciar_sala3(inv, origen=None):
     if inv is None:
         inv = crear_inventario()
+    config = get_config_sala("sala3")
     objetos_sala = []
     # Si viene de sala4, pasar flag especial a cargar_sala
     if origen == "sala4":
@@ -17,7 +19,8 @@ def iniciar_sala3(inv, origen=None):
             objetos_sala=objetos_sala,
             puerta_bloqueada=True,
             mensaje_bloqueo="La puerta está bloqueada. Necesitas algo para romperla.",
-            origen=origen
+            origen=origen,
+            pos_incial=config["puertas"]["izquierda"].topleft
         )
     return cargar_sala(
         "sala3",
